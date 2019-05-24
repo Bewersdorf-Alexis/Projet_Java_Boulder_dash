4a9261aba6858f0ae275323da1570f1d972fc0c6
Florian <florian.carlu@viacesi.fr>
2019-05-24 11:56:39 +0200

    :art: adjust form of code
    adapt methode name and some doc

---
+++ JPU-BlankProject/entity/src/main/java/entity/Level.java
@@ -0,0 +1,97 @@
+package entity;
+
+/**
+ * The Class HelloWorld.
+ *
+ * @author Jean-Aymeric Diet
+ */
+public class Level extends Entity {
+
+	/** The id. */
+	private int			id;
+
+	/** The Map */
+	private String Map;
+	
+	/** The Level. */
+	private String[][]	level;
+
+	/**
+	 * Instantiates a new Level.
+	 *
+	 * @param id
+	 *          the id
+
+	 * @param message
+	 *          the message
+	 */
+	public Level(final int id, final String map) {
+		this.setId(id);
+		this.setMap(map);
+	}
+
+	/**
+	 * Instantiates a new hello world.
+	 */
+	public Level() {
+		this(0, "");
+	}
+
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
+	public int getId() {
+		return this.id;
+	}
+
+	/**
+	 * Sets the id.
+	 *
+	 * @param id
+	 *          the new id
+	 */
+	public void setId(final int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the level.
+	 *
+	 * @return the level
+	 */
+	public String getLevel(int x, int y) {
+		return this.level[x][y];
+	}
+
+	/**
+	 * Sets the level.
+	 *
+	 * @param caracter
+	 *          the new character
+	 */
+	public void setLevel(int x, int y, String character) {
+		this.level[x][y] = character;
+	}
+
+	/**
+	 * Gets the map.
+	 *
+	 * @return the map
+	 */
+	public String getMap() {
+		return Map;
+	}
+
+	/**
+	 * Sets the map.
+	 *
+	 * @param map
+	 *          the new map
+	 */
+	public void setMap(String map) {
+		this.Map = map;
+	}
+
+}
