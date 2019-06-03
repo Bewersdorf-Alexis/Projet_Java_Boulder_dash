e16ae3f8c91488b51958463463ce5953373f9064
Alexis Bewersdorf <alexis.bewersdorf@gmail.com>
2019-06-03 10:53:29 +0200

    :construction: Add Rock JavaDoc

--- JPU-BlankProject/model/src/main/java/model/element/mobile/Rock.java
+++ JPU-BlankProject/model/src/main/java/model/element/mobile/Rock.java
@@ -26,6 +26,14 @@ public class Rock implements IElement {
 	private static Image image;	
 	private String imageName = "Rocher";
 	
+	/**
+     * constructor to build and place Rock
+     *
+     * @param x
+     * @param y
+     * @param LevelMap
+     *         
+     */
 	public Rock(final int x, final int y, LevelMap levelMap) {
 		this.setX(x);
 		this.setY(y);
@@ -35,30 +43,63 @@ public class Rock implements IElement {
 
 	}
 	
+	/**
+     * Get x position of Rock
+     *
+     * @return x
+     * 
+     * @See contract.IElement#getX
+     */
 	@Override
 	public int getX() {
 
 		return this.x;
 	}
 
+	/**
+     * Set x position of Rock
+     *
+     * @param x
+     *  
+     *  @See contract.IElement#setX
+     */
 	@Override
 	public void setX(int x) {
 
 		this.x = x;
 	}
 
+	/**
+     * Get y position of Rock
+     *
+     * @return y
+     * 
+     *  @See contract.IElement#getY
+     */
 	@Override
 	public int getY() {
 
 		return this.y;
 	}
 
+	/**
+     * Set y position of Rock
+     *
+     * @param y
+     * 
+     *  @See contract.IElement#setY
+     */
 	@Override
 	public void setY(int y) {
 
 		this.y = y;
 	}
 	
+	/**
+     * Move up the player when up key are pressed
+     *     
+     *  @See contract.IElement#moveUp
+     */
 	@Override
 	public void moveUp() {
 		
@@ -69,6 +110,12 @@ public class Rock implements IElement {
 
 	}
 	
+	/**
+     * Move down the player when down key are pressed
+     *
+     *  @See contract.IElement#moveDown   
+     */
+	@Override
 	public void moveDown() {
 		this.setY(this.getY() + 1);
 		
@@ -76,13 +123,23 @@ public class Rock implements IElement {
 		this.levelmap.removeElement(getX(), getY()-1);
 	}
 	
+	/**
+     * Move left the player when left key are pressed
+     * 
+     *  @See contract.IElement#moveLeft
+     */
 	public void moveLeft() {
 		this.setX(this.getX() - 1);
 		
 		this.levelmap.setElement(this.getX(), this.getY(), this);
 		this.levelmap.removeElement(getX()+1, getY());
 	}
-	
+	/**
+     * Move right the player when right key are pressed
+     * 
+     *  @See contract.IElement#moveRight
+     */
+	@Override
 	public void moveRight() {
 		this.setX(this.getX() + 1);
 		
@@ -90,24 +147,49 @@ public class Rock implements IElement {
 		this.levelmap.removeElement(getX()-1, getY());
 	}
 	
+	/**
+     * do nothing the player when player don't move
+     * 
+     *  @See contract.IElement#doNothing     
+     */
+	@Override
 	public void doNothing() {
 		this.setY(this.getY());
 		
 		this.levelmap.setElement(this.getX(), this.getY(), this);
 	}
 
+	/**
+     * Get image of Rock
+     *
+     * @return image
+     * 
+     *  @See contract.IElement#getImage        
+     */
 	@Override
 	public Image getImage() {
 
 		return Rock.image;
 	}
 
+	/**
+     * Set image of Rock
+     *
+     * @param image
+     *  
+     *  @See contract.IElement#setImage        
+     */
 	@Override
 	public void setImage(Image image) {
 
 		Rock.image = image;
 	}
 
+	/**
+     * Load image of Rock
+     * 
+     *  @See contract.IElement#loadImage        
+     */
 	@Override
 	public void loadImage() {
 
@@ -121,54 +203,123 @@ public class Rock implements IElement {
 		this.setImage(img);
 	}
 
+	/**
+     * Get image name of Rock
+     *
+     * @return imgaName
+     * 
+     *  @See contract.IElement#getImageName       
+     */
 	@Override
 	public String getImageName() {
 
 		return this.imageName;
 	}
 
+	/**
+     * Set image name of Rock
+     *
+     * @param imageName
+     * 
+     *  @See contract.IElement#setImageName        
+     */
 	@Override
 	public void setImageName(String imageName) {
 
 		this.imageName = imageName;
 	}
 
-
+	/**
+     * check existing of Rock
+     *
+     * @return exist
+     * 
+     *  @See contract.IElement#isExist
+     */
 	@Override
 	public boolean isExist() {
 
 		return this.exist;
 	}
 
+	/**
+     * set exist verification of Rock
+     *
+     * @param exist
+     * 
+     *  @See contract.IElement#setExist
+     */
 	@Override
 	public void setExist(boolean exist) {
 		this.exist = exist;
 	}
 
-
+	/**
+     * Get level
+     *
+     * @return level map
+     * 
+     *  @See contract.IElement#getLevelmap
+     */
 	public ILevelMap getLevelmap() {
 		return levelmap;
 	}
 
+	/**
+     * Set level
+     *
+     * @param levelMap
+     * 
+     *  @See contract.IElement#setLevelmap        
+     */
 	public void setLevelmap(ILevelMap levelmap) {
 		this.levelmap = levelmap;
 	}
 
+	/**
+     * Get score of collected diamond
+     *
+     * @return score
+     * 
+     * @See contract.IElement#getScore
+     */
 	@Override
 	public int getScore() {
 		return score;
 	}
 
+	/**
+     * Set score of collected diamond
+     *
+     * @param score
+     * 
+     *  @See contract.IElement#setScore        
+     */
 	@Override
 	public void setScore(int score) {
 		this.score = score;
 	}
 
+	/**
+     * Get element type of Rock
+     *
+     * @return element type
+     * 
+     *  @See contract.IElement#getElementType       
+     */
 	@Override
 	public ElementType getElementType() {
 		return elementType;
 	}
 
+	/**
+     * Set element type of Rock
+     *
+     * @param elementType
+     * 
+     *  @See contract.IElement#setElementType        
+     */
+	@Override
 	public void setElementType(ElementType elementType) {
 		this.elementType = elementType;
 	}
