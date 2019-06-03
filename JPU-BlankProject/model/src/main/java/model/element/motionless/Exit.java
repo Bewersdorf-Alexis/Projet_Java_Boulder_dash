c3f41c9c5e4f78bb72e6d83ccdc954bdddbf2dac
Alexis Bewersdorf <alexis.bewersdorf@gmail.com>
2019-06-03 11:13:54 +0200

    :construction: Add Exit JavaDoc

--- JPU-BlankProject/model/src/main/java/model/element/motionless/Exit.java
+++ JPU-BlankProject/model/src/main/java/model/element/motionless/Exit.java
@@ -26,6 +26,14 @@ public class Exit implements IElement {
 	private static Image image;	
 	private String imageName = "trapdoor";
 	
+	/**
+     * constructor to build and place Exit
+     *
+     * @param x
+     * @param y
+     * @param LevelMap
+     *         
+     */
 	public Exit(final int x, final int y, LevelMap levelMap) {
 		this.setX(x);
 		this.setY(y);
@@ -35,42 +43,64 @@ public class Exit implements IElement {
 
 	}
 	
+	/**
+     * Get x position of Exit
+     *
+     * @return x
+     * 
+     * @See contract.IElement#getX
+     */
 	@Override
 	public int getX() {
 
 		return this.x;
-		/**
-	       * Get x position of exit
-	       *
-	       * @param x
-	       *         
-	       */
+		
 	}
-
+	
+	/**
+     * Set x position of Exit
+     *
+     * @param x
+     *         
+     */
 	@Override
 	public void setX(int x) {
 
 		this.x = x;
-		/**
-	       * Set x position of block
-	       *
-	       * @param x
-	       *         
-	       */
+		
 	}
 
+	/**
+     * Get y position of Exit
+     *
+     * @return y
+     * 
+     *  @See contract.IElement#getY
+     */
 	@Override
 	public int getY() {
 
 		return this.y;
 	}
 
+	/**
+     * Set y position of Exit
+     *
+     * @param y
+     * 
+     *  @See contract.IElement#setY
+     */
 	@Override
 	public void setY(int y) {
 
 		this.y = y;
 	}
 	
+	/**
+     * Move up the player when up key are pressed
+     *     
+     *  @See contract.IElement#moveUp
+     */
 	@Override
 	public void moveUp() {
 		
@@ -81,6 +111,12 @@ public class Exit implements IElement {
 
 	}
 	
+	/**
+     * Move down the player when down key are pressed
+     *
+     *  @See contract.IElement#moveDown   
+     */
+	@Override
 	public void moveDown() {
 		this.setY(this.getY() + 1);
 		
@@ -88,6 +124,12 @@ public class Exit implements IElement {
 		this.levelmap.removeElement(getX(), getY()-1);
 	}
 	
+	/**
+     * Move left the player when left key are pressed
+     * 
+     *  @See contract.IElement#moveLeft
+     */
+	@Override
 	public void moveLeft() {
 		this.setX(this.getX() - 1);
 		
@@ -95,6 +137,12 @@ public class Exit implements IElement {
 		this.levelmap.removeElement(getX()+1, getY());
 	}
 	
+	/**
+     * Move right the player when right key are pressed
+     * 
+     *  @See contract.IElement#moveRight
+     */
+	@Override
 	public void moveRight() {
 		this.setX(this.getX() + 1);
 		
@@ -102,24 +150,49 @@ public class Exit implements IElement {
 		this.levelmap.removeElement(getX()-1, getY());
 	}
 	
+	/**
+     * do nothing the player when player don't move
+     * 
+     *  @See contract.IElement#doNothing     
+     */
+	@Override
 	public void doNothing() {
 		this.setY(this.getY());
 		
 		this.levelmap.setElement(this.getX(), this.getY(), this);
 	}
 
+	/**
+     * Get image of Exit
+     *
+     * @return image
+     * 
+     *  @See contract.IElement#getImage        
+     */
 	@Override
 	public Image getImage() {
 
 		return Exit.image;
 	}
 
+	/**
+     * Set image of Exit
+     *
+     * @param image
+     *  
+     *  @See contract.IElement#setImage        
+     */
 	@Override
 	public void setImage(Image image) {
 
 		Exit.image = image;
 	}
 
+	/**
+     * Load image of Exit
+     * 
+     *  @See contract.IElement#loadImage        
+     */
 	@Override
 	public void loadImage() {
 
@@ -133,54 +206,123 @@ public class Exit implements IElement {
 		this.setImage(img);
 	}
 
+	/**
+     * Get image name of Exit
+     *
+     * @return imgaName
+     * 
+     *  @See contract.IElement#getImageName       
+     */
 	@Override
 	public String getImageName() {
 
 		return this.imageName;
 	}
 
+	/**
+     * Set image name of Exit
+     *
+     * @param imageName
+     * 
+     *  @See contract.IElement#setImageName        
+     */
 	@Override
 	public void setImageName(String imageName) {
 
 		this.imageName = imageName;
 	}
 
-
+	/**
+     * check existing of Exit
+     *
+     * @return exist
+     * 
+     *  @See contract.IElement#isExist
+     */
 	@Override
 	public boolean isExist() {
 
 		return this.exist;
 	}
 
+	/**
+     * set exist verification of Exit
+     *
+     * @param exist
+     * 
+     *  @See contract.IElement#setExist
+     */
 	@Override
 	public void setExist(boolean exist) {
 		this.exist = exist;
 	}
 
-
+	/**
+     * Get level
+     *
+     * @return level map
+     * 
+     *  @See contract.IElement#getLevelmap
+     */
 	public ILevelMap getLevelmap() {
 		return levelmap;
 	}
 
+	/**
+     * Set level
+     *
+     * @param levelMap
+     * 
+     *  @See contract.IElement#setLevelmap        
+     */
 	public void setLevelmap(ILevelMap levelmap) {
 		this.levelmap = levelmap;
 	}
 
+	/**
+     * Get score of collected diamond
+     *
+     * @return score
+     * 
+     * @See contract.IElement#getScore
+     */
 	@Override
 	public int getScore() {
 		return score;
 	}
 
+	/**
+     * Set score of collected diamond
+     *
+     * @param score
+     * 
+     *  @See contract.IElement#setScore        
+     */
 	@Override
 	public void setScore(int score) {
 		this.score = score;
 	}
 
+	/**
+     * Get element type of diamond
+     *
+     * @return element type
+     * 
+     *  @See contract.IElement#getElementType       
+     */
 	@Override
 	public ElementType getElementType() {
 		return elementType;
 	}
 
+	/**
+     * Set element type of diamond
+     *
+     * @param elementType
+     * 
+     *  @See contract.IElement#setElementType        
+     */
+	@Override
 	public void setElementType(ElementType elementType) {
 		this.elementType = elementType;
 	}
